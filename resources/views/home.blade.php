72ce3a9c5b5b3f52125a892cac731bdbcdf6a918
ahmed ashraf <ahmedashrafhero@gmail.com>
2020-07-03 18:50:23 +0200

    firstCommitt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,7 +75,7 @@
             if(e.keyCode==13){
                 globalChannel.sendMessage($('#msg').val());
                 $("#outgoing").animate({ scrollTop: 200000000 }, "slow");
-                $('#msg').val('')
+                $('#msg').val('')//asd
             }
         })
 
