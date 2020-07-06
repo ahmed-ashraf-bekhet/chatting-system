eab054bfdf9f4d93dc6319ed18d679be37d628db
ahmed-ashraf-bekhet <ahmed.ashraaf.97@gmail.com>
2020-07-06 04:25:45 -0700

    firstCommit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,22 +84,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    Real Time Chatting System
                 </div>
             </div>
         </div>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-        
+
     </body>
 </html>
